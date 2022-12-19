28d008422dc27032b9b63d141f177d127465b71d
michaelv24 <mvsch20@gmail.com>
2022-12-19 10:16:58 -0500

    Arrays Created

---
+++ vite-project/js/array.js
@@ -0,0 +1,56 @@
+const items = [
+  {
+    name: "Pokemon Scarlet and Violet",
+    type: "Video Game",
+    company: "Nintendo",
+    console: "Nintendo Switch",
+  },
+  {
+    name: "New Pokemon Snap",
+    type: "Video Game",
+    company: "Nintendo",
+    console: "Nintendo Switch",
+  },
+  {
+    name: "Pokemon Legends Arceus",
+    type: "Video Game",
+    company: "Nintendo",
+    console: "Nintendo Switch",
+  },
+  {
+    name: "Mouse",
+    type: "Electronic",
+    company: "Logitech",
+    console: "PC",
+  },
+  {
+    name: "Keyboard",
+    type: "Electronic",
+    company: "Logitech",
+    console: "PC",
+  },
+  {
+    name: "Headset",
+    type: "Electronic",
+    company: "Logitech",
+    console: "PC",
+  },
+  {
+    name: "Monkey D. Luffy Figure",
+    type: "Figure",
+    company: "Crunchyroll",
+    console: "Anime Collectible",
+  },
+  {
+    name: "Deku My Hero Academia Figure",
+    type: "Figure",
+    company: "Crunchyroll",
+    console: "Anime Collectible",
+  },
+  {
+    name: "Naruto Figure",
+    type: "Figure",
+    company: "Crunchyroll",
+    console: "Anime Collectible",
+  },
+];
