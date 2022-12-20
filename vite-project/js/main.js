8dfe29f1ada74976ca97de1420eb9e59e55ea89b
michaelv24 <mvsch20@gmail.com>
2022-12-20 10:16:45 -0500

    linked css

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,4 @@
-import "../style./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "../counter.js";
+import "./style.css";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -21,3 +19,12 @@ document.querySelector("#app").innerHTML = `
 `;
 
 setupCounter(document.querySelector("#counter"));
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
+});
